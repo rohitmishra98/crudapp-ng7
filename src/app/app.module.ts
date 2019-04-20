23bd8367a9e356f3a4ce58829e5a78ec8309b451
Rohit Mishra <rohitmishra9869@gmail.com>
2019-04-21 03:42:44 +0530

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TableListComponent, FilterPipe } from './table-list/table-list.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { RouterModule } from '@angular/router';
+import {FormsModule,  FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableListComponent,
+    AddEmployeeComponent,
+    FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    AppRoutingModule,
+    ModalModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
